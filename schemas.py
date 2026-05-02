87e1a888b496d73f8b59d2a85a677f470fd0e54d
Caroline Nabeshima <c.miyako@gmail.com>
2026-05-02 19:49:26 +0900

    feat: add NoteCreate Pydantic schema to validate notes

---
+++ schemas.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class NoteCreate(BaseModel):
+    category: str = Field(max_length=50)
+    message_body: str = Field(max_length=1000)
+    tag: Optional[str] = Field(default=None, max_length=50)
