a1dea23ba0f2dc5333d94b72a0fd46e614cdd7c8
Caroline Nabeshima <c.miyako@gmail.com>
2026-05-02 23:34:03 +0900

    feat: add create note endpoint

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from fastapi import FastAPI
+from sqlalchemy.orm import Session
 import models
-from database import engine
+from fastapi import FastAPI, Depends
+from models import Note
+from database import engine, SessionLocal
+from schemas import NoteCreate
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
-     return {"mensagem": "API do Diário Digital está viva!"}
+     return {"message": "Digital Diary API is alive!"}
+
+@app.post("/notes/")
+def create_note(payload: NoteCreate, db: Session = Depends(get_db)):
+    note = Note(
+        category=payload.category,
+        message_body=payload.message_body,
+        tag=payload.tag
+        )
+    db.add(note)
+    db.commit()
+    db.refresh(note)
+    return note
+
+    
