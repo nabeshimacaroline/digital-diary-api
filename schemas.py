d0da467b8df3958bce0fe476a2fac1361841bac2
Caroline Nabeshima <c.miyako@gmail.com>
2026-05-03 11:09:12 +0900

    feat: add NoteUpdate schema

--- schemas.py
+++ schemas.py
@@ -5,3 +5,8 @@ class NoteCreate(BaseModel):
     category: str = Field(max_length=50)
     message_body: str = Field(max_length=1000)
     tag: Optional[str] = Field(default=None, max_length=50)
+
+class NoteUpdate(BaseModel):
+    category: Optional[str] = Field(default=None, max_length=50)
+    message_body: Optional[str] = Field(default=None, max_length=1000)
+    tag: Optional[str] = Field(default=None, max_length=50)
