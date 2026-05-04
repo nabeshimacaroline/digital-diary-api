0e48a4f3c68f8ca864465896b41b5258e70fe9f8
Caroline Nabeshima <c.miyako@gmail.com>
2026-05-04 10:52:11 +0900

    feat: add update note endpoint

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import models
 from fastapi import FastAPI, Depends, HTTPException
 from models import Note
 from database import engine, SessionLocal
-from schemas import NoteCreate
+from schemas import NoteCreate, NoteUpdate
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -42,4 +42,21 @@ def get_note(note_id: int, db: Session = Depends(get_db)):
     note = db.query(Note).filter(Note.id == note_id).first()
     if note is None:
         raise HTTPException(status_code=404, detail="Note not found")
+    return note
+
+@app.patch("/notes/{note_id}")
+def update_note(note_id: int, payload: NoteUpdate, db: Session = Depends(get_db)):
+    # buscar e validar
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    #atualizar dados
+    update_data = payload.model_dump(exclude_unset=True) # Pega só o que o usuário enviou
+    for key, value in update_data.items():
+        setattr(note, key, value) # Substitui o valor antigo pelo novo no objeto da nota
+    
+    #salvar e retornar
+    db.commit()
+    db.refresh(note)
     return note
