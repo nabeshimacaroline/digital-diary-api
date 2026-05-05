2d6153f740045c02b2a7f0dc21e515b28b9eaca7
Caroline Nabeshima <c.miyako@gmail.com>
2026-05-05 12:11:02 +0900

    feat: add EventCreate schema

--- schemas.py
+++ schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from datetime import datetime
+
 
 class NoteCreate(BaseModel):
     category: str = Field(max_length=50)
@@ -10,3 +12,11 @@ class NoteUpdate(BaseModel):
     category: Optional[str] = Field(default=None, max_length=50)
     message_body: Optional[str] = Field(default=None, max_length=1000)
     tag: Optional[str] = Field(default=None, max_length=50)
+
+class EventCreate(BaseModel):
+    category: str = Field(max_length=50)
+    message_body: str = Field(max_length=1000)
+    classification: str = Field(max_length=50)
+    tag: Optional[str] = Field(default=None, max_length=50)
+    scheduled_at: datetime
+    notification_at: Optional[datetime]= Field(default=None)
