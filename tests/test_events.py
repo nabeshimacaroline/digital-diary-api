59af23025b7426f9996a500ce0d528ed41d9f1ce
Caroline Nabeshima <c.miyako@gmail.com>
2026-07-06 09:58:09 +0900

    test(events): add success test for event creation and enforce default status rule
    - Add test_create_event_success to validate successful POST /events.
    - Validate business rule ensuring the default initial status is correctly applied.
    - Remove hardcoded status and use StatusEvent.SCHEDULED as the single source of truth.

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,5 +1,6 @@
 import pytest
 from freezegun import freeze_time
+from app.enums import StatusEvent
 
 @pytest.fixture(autouse=True)
 def freeze_time_for_events():
@@ -30,3 +31,23 @@ def test_create_event_in_past_date(authorized_client):
     assert event_attempt.status_code == 400
     assert event_attempt.json()["detail"] == "Scheduled cannot be a date in the past."
 
+def test_creat_event_success(authorized_client):
+# ==========================================
+# 1. ARRANGE
+# ==========================================
+    event_payload = {
+        "category": "projeto",
+        "message_body": "Teste caminho feliz para criação de eventos",
+        "classification": "tarefa",
+        "scheduled_at": "2026-07-08T09:00:00Z"
+    }
+# ==========================================
+# 2. ACT
+# ==========================================
+    event_attempt = authorized_client.post("/events/", json=event_payload)
+
+# ==========================================
+# 3. ASSERT
+# ==========================================
+    assert event_attempt.status_code == 201
+    assert event_attempt.json()["status"] == StatusEvent.SCHEDULED
